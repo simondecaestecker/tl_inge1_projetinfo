8cfe62e22f23dad247d6cc68316534fd95b400a7
Arthur <alouchart@elv.telecom-lille.fr>
2016-05-20 15:38:23 +0200

    Ajout classe : Groupe et Utilisateur

--- src/user/userModel/Groupe.java
+++ src/user/userModel/Groupe.java
@@ -2,4 +2,27 @@ package userModel;
 
 public class Groupe {
 
+	private int id;
+	private int nombre;
+	
+	Groupe(int id){
+		this.id = id;	
+		this.nombre = 0;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public int getNombre(){
+		return nombre;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public void setNombre(int nombre){
+		this.nombre = nombre;
+	}
 }
--- src/user/userModel/Utilisateur.java
+++ src/user/userModel/Utilisateur.java
@@ -1,5 +1,18 @@
 package userModel;
 
 public class Utilisateur {
+	private String login;
+	private String password;
+	private String nom;
+	private String prenom;
+	private int role;
+	
+	Utilisateur(String login, String password, String nom, String prenom, int role){
+		this.login = login;
+		this.password = password;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.role = role;
+	}
 
 }
