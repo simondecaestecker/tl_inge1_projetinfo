257a156e21b54d3383295148a35658b09614f661
Simon <sdecaestecker@elv.telecom-lille.fr>
2016-05-25 14:56:54 +0200

    Méthode createUser

--- src/user/userModel/Administrateur.java
+++ src/user/userModel/Administrateur.java
@@ -1,12 +1,27 @@
 package userModel;
 
 public class Administrateur extends Utilisateur {
-	public Administrateur(String login, String password, String nom, String prenom, int role){
-		super(login, password, nom, prenom, role);
+	public Administrateur(String login, String password, String nom, String prenom, int id){
+		super(login, password, nom, prenom, id);
 	}
-
-	private boolean createUser() {
-		return false;
+	
+	public boolean createUser(String login, String password, String nom, String prenom, int id, int type) {
+		if (type == 0) {
+			//Administrateur
+			Administrateur newUser = new Administrateur(login, password, nom, prenom, id);
+			return true;
+		}
+		else if (type == 1) {
+			Professeur newUser = new Professeur(login, password, nom, prenom, /*1000 + */id);
+			return true;
+		}
+		else if (type == 2) {
+			Etudiant newUser = new Etudiant(login, password, nom, prenom, /*2000 + */id);
+			return true;
+		}
+		else {
+			return false;
+		}		
 	}
 	
 	private boolean createGroup() {
