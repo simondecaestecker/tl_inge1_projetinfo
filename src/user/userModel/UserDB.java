90ee859393864da97fe376bff45e7eaf76a9796b
Simon <sdecaestecker@elv.telecom-lille.fr>
2016-06-01 16:10:01 +0200

    Ajout de compléments dans saveDB

--- src/user/userModel/UserDB.java
+++ src/user/userModel/UserDB.java
@@ -88,21 +88,21 @@ public class UserDB {
 
 	public boolean loadDB(){
 		org.jdom2.Document document = null ;
-		
+
 		Element rootElt;
 		SAXBuilder sxb = new SAXBuilder();
-		
+
 		try{
 			document = sxb.build(new File("userDB.xml"));
 		}catch(Exception e){}
-		
+
 		if(document!=null){
 			rootElt = document.getRootElement();
-			
+
 			Element groupsDB = rootElt.getChild("Groups");
 			List<Element> groupsElmts = groupsDB.getChildren("Group");
 			Iterator<Element> itGroup = groupsElmts.iterator();
-			
+
 			while(itGroup.hasNext()){
 				Element unGroupElmt = (Element)itGroup.next();
 
@@ -114,7 +114,7 @@ public class UserDB {
 			Element studentsDB = rootElt.getChild("Students");
 			List<Element> studentsElmts = studentsDB.getChildren("Student");
 			Iterator<Element> itStudent = studentsElmts.iterator();
-			
+
 			while(itStudent.hasNext()){
 				Element unStudentElmt = (Element)itStudent.next();
 
@@ -128,7 +128,7 @@ public class UserDB {
 			Element teachersDB = rootElt.getChild("Teachers");
 			List<Element> teachersElmts = teachersDB.getChildren("Teacher");
 			Iterator<Element> itTeacher = teachersElmts.iterator();
-			
+
 			while(itTeacher.hasNext()){
 				Element unTeacherElmt = (Element)itTeacher.next();
 
@@ -151,26 +151,96 @@ public class UserDB {
 				utilisateurs.put(login, administrator);
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	public boolean saveDB(){
 		Element rootElmt = new Element("UsersDB");
 		org.jdom2.Document document = new Document(rootElmt);
-		
+
 		Element groupsElmts = new Element("Groups");
 		rootElmt.setContent(groupsElmts);
-		
+
 		Element studentsElmts = new Element("Students");
 		rootElmt.setContent(studentsElmts);
-		
+
 		Element teachersElmts = new Element("Teachers");
 		rootElmt.setContent(teachersElmts);
-		
+
 		Element administratorsElmts = new Element("Administrators");
 		rootElmt.setContent(administratorsElmts);
-	
+
+		//Save les Groups
+		for (Entry<Integer, Groupe> entry : groupes.entrySet()) {
+			Integer cle = entry.getKey();
+			Groupe valeur = entry.getValue();
+
+			Element unGroup = new Element("Group");
+			groupsElmts.setContent(unGroup);
+
+			Element unGroupId = new Element("groupId");
+			unGroup.addContent(unGroupId);
+			unGroupId.setText(Integer.toString(valeur.getId()));
+		}
+
+		//Save les Utilisateurs
+		for (Entry<String, Utilisateur> entry : utilisateurs.entrySet()) {
+			String cle = entry.getKey();
+			Utilisateur valeur = entry.getValue();
+
+			Element unUser = new Element(valeur.getClassUser());
+
+			Element unUserLogin = new Element("login");
+			unUser.addContent(unUserLogin);
+			unUserLogin.setText(valeur.getLogin());
+
+			Element unUserFirstname = new Element("firstname");
+			unUser.addContent(unUserFirstname);
+			unUserFirstname.setText(valeur.getFirstName());
+
+			Element unUserSurname = new Element("surname");
+			unUser.addContent(unUserSurname);
+			unUserSurname.setText(valeur.getSurname());
+
+			Element unUserPwd = new Element("pwd");
+			unUser.addContent(unUserPwd);
+			unUserPwd.setText(valeur.getPassword());
+
+			Element unUserId;
+
+			if (valeur.getClassUser() == "Student") {
+				studentsElmts.setContent(unUser);
+
+				unUserId = new Element("studentId");
+
+				Element unUserGroupId = new Element("groupId");
+				unUser.addContent(unUserGroupId);
+				unUserGroupId.setText(Integer.toString(((Etudiant) valeur).getIdGroupe()));
+			}
+
+			else if (valeur.getClassUser() == "Teacher") {
+				teachersElmts.setContent(unUser);
+
+				unUserId = new Element("teacherId");
+			}
+
+			else if (valeur.getClassUser() == "Administrator") {
+				administratorsElmts.setContent(unUser);
+
+				unUserId = new Element("adminId");
+			}
+
+			else {
+				return false;
+			}
+
+			unUserId.setText(Integer.toString(valeur.getId()));
+			unUser.addContent(unUserId);
+
+			return true;
+		}		
+
 		return true;
 	}
 }
